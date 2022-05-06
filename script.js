1fa09fa1ca41913eb123e73d308e8e4256d9a652
kennyfmx <fmxhighlife@gmail.com>
2022-05-06 17:07:05 +0300

    fix: change content-wrapper const name to header-content-wrapper

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const HEADER = document.createElement('header');
   HEADER.classList.add('header');
   const MAIN = document.createElement('main');
-  const CONTENT_WRAPPER = document.createElement('div');
-  CONTENT_WRAPPER.classList.add('content-wrapper');
+  const HEADER_CONTENT_WRAPPER = document.createElement('div');
+  const MAIN_CONTENT_WRAPPER = document.createElement('div');
+  HEADER_CONTENT_WRAPPER.classList.add('content-wrapper');
+  MAIN_CONTENT_WRAPPER.classList.add('content-wrapper');
   BODY.prepend(HEADER);
   HEADER.after(MAIN);
 
-  HEADER.appendChild(CONTENT_WRAPPER);
+  HEADER.appendChild(HEADER_CONTENT_WRAPPER);
   let title = document.createElement('h1');
   title.classList.add('title', 'text');
   title.innerText = "Virtual Keyboard";
-  CONTENT_WRAPPER.appendChild(title);
+  HEADER_CONTENT_WRAPPER.appendChild(title);
   let description = document.createElement('h2');
   description.classList.add('description', 'text');
   description.innerText = "Created for Windows operating system";
-  CONTENT_WRAPPER.appendChild(description);
+  HEADER_CONTENT_WRAPPER.appendChild(description);
+
+
+
 });
