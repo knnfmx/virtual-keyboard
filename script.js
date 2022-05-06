45c0200e21c568377087f587a4fd99047aac146d
kennyfmx <fmxhighlife@gmail.com>
2022-05-06 18:36:59 +0300

    feat: add textarea, keyboard-container with key rows

--- script.js
+++ script.js
@@ -11,15 +11,46 @@ document.addEventListener('DOMContentLoaded', () => {
   BODY.prepend(HEADER);
   HEADER.after(MAIN);
 
-  HEADER.appendChild(HEADER_CONTENT_WRAPPER);
+  HEADER.append(HEADER_CONTENT_WRAPPER);
+
   let title = document.createElement('h1');
   title.classList.add('title', 'text');
   title.innerText = "Virtual Keyboard";
-  HEADER_CONTENT_WRAPPER.appendChild(title);
+  HEADER_CONTENT_WRAPPER.append(title);
+
   let description = document.createElement('h2');
   description.classList.add('description', 'text');
   description.innerText = "Created for Windows operating system";
-  HEADER_CONTENT_WRAPPER.appendChild(description);
+  HEADER_CONTENT_WRAPPER.append(description);
+
+  MAIN.append(MAIN_CONTENT_WRAPPER);
+
+  let textArea = document.createElement('textarea');
+  textArea.id = 'textarea';
+  textArea.setAttribute('autofocus', true);
+  textArea.classList.add('textarea');
+  MAIN_CONTENT_WRAPPER.append(textArea);
+
+  let keyboard = document.createElement('div');
+  keyboard.classList.add('keyboard');
+  MAIN_CONTENT_WRAPPER.append(keyboard);
+
+  let keysRowOne = document.createElement('div');
+  keysRowOne.classList.add('keyboard__rows');
+
+  let keysRowTwo = document.createElement('div');
+  keysRowTwo.classList.add('keyboard__rows');
+
+  let keysRowThree = document.createElement('div');
+  keysRowThree.classList.add('keyboard__rows');
+
+  let keysRowFour = document.createElement('div');
+  keysRowFour.classList.add('keyboard__rows');
+
+  let keysRowFive = document.createElement('div');
+  keysRowFive.classList.add('keyboard__rows');
+
+  keyboard.append(keysRowOne, keysRowTwo, keysRowThree, keysRowFour, keysRowFive);
 
 
 
