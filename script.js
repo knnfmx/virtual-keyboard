e90fe1cf2582894356c3d3add314557b8b0a2d5c
kennyfmx <fmxhighlife@gmail.com>
2022-05-09 17:30:43 +0300

    feat: add eng keyboard structure with styles

--- script.js
+++ script.js
@@ -35,9 +35,23 @@ document.addEventListener('DOMContentLoaded', () => {
   keyboard.classList.add('keyboard');
   MAIN_CONTENT_WRAPPER.append(keyboard);
 
+  
+  let keyboardKeys = {
+    firstKeysRow: ['`', '1', '2','3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
+    firstKeysRowShift: ['~', '!', '@','#', '$', '%', '^', '&', '*', '(', ')', '_', '+', ''],
+    secondKeysRow: ['tab', 'q', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'del', 'enter'],
+    secondKeysRowShift: ['', 'Q', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '', ''],
+    thirdKeysRow: ['caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\\'],
+    thirdKeysRowShift: ['', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\"', '|'],
+    fourthKeysRow: ['shiftLeft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑','shiftRight'],
+    fourthKeysRowShift: ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '',''],
+    fifthKeysRow: ['ctrl', 'start', 'alt', 'whitespace', 'alt', 'fn', '←', '↓', '→', 'ctrl'],
+    // fifthKeysRowShift: ['ctrl', 'start', 'alt', 'whitespace', 'alt', 'fn', 'left', 'down', 'right', 'ctrl'],
+  };
+
   let keysRowOne = document.createElement('div');
   keysRowOne.classList.add('keyboard__rows');
-
+  
   let keysRowTwo = document.createElement('div');
   keysRowTwo.classList.add('keyboard__rows');
 
@@ -52,6 +66,92 @@ document.addEventListener('DOMContentLoaded', () => {
 
   keyboard.append(keysRowOne, keysRowTwo, keysRowThree, keysRowFour, keysRowFive);
 
-
-
+  function init(arr, row, arrShift) {
+    let out = '';
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == 'backspace') {
+        out += `<div class="rows__key backspace">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == 'del') {
+        out += `<div class="rows__key del">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == 'whitespace') {
+        out += `<div class="rows__key whitespace">
+              </div>`;
+      } else if (arr[i] == 'enter') {
+        out += `<div class="rows__key enter">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == 'shiftLeft') {
+        out += `<div class="rows__key shift-left">
+                <span class="key__lang_active">shift</span>
+              </div>`;
+      } else if (arr[i] == 'shiftRight') {
+        out += `<div class="rows__key shift-right">
+                <span class="key__lang_active">shift</span>
+              </div>`;
+      } else if (arr[i] == 'caps') {
+        out += `<div class="rows__key capslock">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == 'tab') {
+        out += `<div class="rows__key tab">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == 'ctrl') {
+        out += `<div class="rows__key ctrl">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == 'alt') {
+        out += `<div class="rows__key alt">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == 'start') {
+        out += `<div class="rows__key start">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == 'fn') {
+        out += `<div class="rows__key fn">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else if (arr[i] == '↑' || arr[i] == '←' || arr[i] == '↓' || arr[i] == '→') {
+        out += `<div class="rows__key arrows">
+                <span class="key__lang_active">${arr[i]}</span>
+              </div>`;
+      } else {
+        out += `<div class="rows__key">
+                  <sup class="key__lang_shift">${arrShift[i]}</sup>
+                  <span class="key__lang_active">${arr[i]}</span>
+                </div>`;
+      }
+    }
+    row.innerHTML = out;
+  }
+  // TODO ADD CLASSES for other btns 
+  function createKey() {
+    for (let key in keyboardKeys) { 
+      console.log(key);
+      if (key == 'firstKeysRow') {
+        init(keyboardKeys[key], keysRowOne, keyboardKeys.firstKeysRowShift);
+      }
+      if (key == 'secondKeysRow') {
+        init(keyboardKeys[key], keysRowTwo, keyboardKeys.secondKeysRowShift);
+      }
+      if (key == 'thirdKeysRow') {
+        init(keyboardKeys[key], keysRowThree, keyboardKeys.thirdKeysRowShift);
+      }
+      if (key == 'fourthKeysRow') {
+        init(keyboardKeys[key], keysRowFour, keyboardKeys.fourthKeysRowShift);
+      }
+      if (key == 'fifthKeysRow') {
+        init(keyboardKeys[key], keysRowFive, );
+      }
+    }
+  }
+  createKey();
+  document.onkeydown= function (ev) {
+    console.log(ev.code);
+  }
 });
